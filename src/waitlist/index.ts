e44a8272661d564ef9413a687d0adf60a9bf5314
Haxurn <haxurn@gmail.com>
2026-02-14 01:28:16 +0300

    feat(waitlist): add waitlist module exports

---
+++ src/waitlist/index.ts
@@ -0,0 +1,12 @@
+export { waitlist } from "./server";
+export { waitlistClient } from "./client";
+export type {
+  WaitlistStatus,
+  WaitlistEntry,
+  WaitlistEntryInput,
+  WaitlistPositionResponse,
+  WaitlistStatusResponse,
+  WaitlistListOptions,
+  WaitlistListResponse,
+  WaitlistPluginOptions,
+} from "./types";
